ae1a537a2ebedeb823a595e3975ef873e9c09caa
asdru0510 <sifontesupta@gmail.com>
2024-08-19 19:44:20 -0500

    realizado modelo de flan y migrado

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from views import index, about, welcome
+from web.views import index, about, welcome
 
 urlpatterns = [
     path("", index, name="index"),
